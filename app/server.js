24149fbb32a7f3cc4a012fc282769fd804332edf
Bharati Subramanian <bharatisharada@gmail.com>
2022-08-04 17:58:17 +0530

    refactor: update server.js folder

--- app/server.js
+++ app/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 require("dotenv").config();
-const routes = require("./routes");
+const routes = require("../routes");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const cors = require("cors");
