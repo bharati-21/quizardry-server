bb0a0cf0aeff186d5c4a889f2b51c78cd91d8ff3
Bharati Subramanian <bharatisharada@gmail.com>
2022-08-16 13:57:09 +0530

    refactor: [rettier formatting

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,8 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { login, signup} = require('../controllers/users-controller');
+const { login, signup } = require("../controllers/users-controller");
 
-router.post('/login', login);
-router.post('/signup', signup);
+router.post("/login", login);
+router.post("/signup", signup);
 
 module.exports = router;
