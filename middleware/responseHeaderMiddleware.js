b11c8377e5d652e5becbce2b26557947f27efd2e
Bharati Subramanian <bharatisharada@gmail.com>
2022-08-24 11:04:09 +0530

    refactor: add control headers

--- middleware/responseHeaderMiddleware.js
+++ middleware/responseHeaderMiddleware.js
@@ -1,6 +1,8 @@
 module.exports = (req, res, next) => {
-	res.setHeader("Access-Control-Allow-Origin", "*");
-	res.setHeader("Access-Control-Allow-Credentials", true);
-
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested, Content-Type, Accept Authorization"
+	);
 	next();
 };
